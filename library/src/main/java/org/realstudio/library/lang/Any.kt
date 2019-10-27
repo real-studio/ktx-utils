2abd46d0af32c377b2c1aff7251c06dea971cea7
yanjie <androidman@yeah.net>
2019-10-28 01:19:14 +0800

    初始化提交

---
+++ library/src/main/java/org/realstudio/library/lang/Any.kt
@@ -0,0 +1,12 @@
+package org.realstudio.library.lang
+
+
+/**
+ * @author         yanjie
+ * @date           2019-10-28 00:25
+ * @version        1.0
+ */
+
+fun Any?.isEmpyt(){
+
+}
---
+++ library/src/main/java/org/realstudio/library/view/View.kt
@@ -0,0 +1,26 @@
+package org.realstudio.library.view
+
+import android.view.View
+
+
+/**
+ * @author         yanjie
+ * @date           2019-10-28 00:00
+ * @version        1.0
+ */
+
+/**
+ * view是否隐藏
+ */
+fun View.visible(visible: Boolean) {
+    this.visibility = if (visible) View.VISIBLE else View.GONE
+}
+
+
+/**
+ * view是否显示
+ */
+fun View.invisible(visible: Boolean) {
+    this.visibility = if (visible) View.VISIBLE else View.INVISIBLE
+}
+
