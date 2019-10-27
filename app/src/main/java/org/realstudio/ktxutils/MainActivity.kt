23dc8b79141ce5d0b130e7f1918fa78f57cdc87e
yanjie <androidman@yeah.net>
2019-10-27 23:41:18 +0800

    first commit

---
+++ app/src/main/java/org/realstudio/ktxutils/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.realstudio.ktxutils
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
